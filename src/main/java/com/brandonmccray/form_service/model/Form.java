f179c33a1b48e07eb5cb7bf770ebed00c95f224e
Brandon McCray <bradmccray89@gmail.com>
2024-12-21 17:32:39 -0500

    adding basic folder structure

---
+++ src/main/java/com/brandonmccray/form_service/model/Form.java
@@ -0,0 +1,22 @@
+package com.brandonmccray.form_service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Form {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @ElementCollection
+    private List<String> fields;
+}
