c2312623c43471b0023c3695e3ea2cfa40b88078
Brandon McCray <bradmccray89@gmail.com>
2024-12-24 23:58:34 -0500

    import performance improvements

--- src/main/java/com/brandonmccray/form_service/model/Form.java
+++ src/main/java/com/brandonmccray/form_service/model/Form.java
@@ -1,6 +1,11 @@
 package com.brandonmccray.form_service.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
