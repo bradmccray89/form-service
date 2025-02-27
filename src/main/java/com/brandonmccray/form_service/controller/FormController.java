dc337a373e884ccb69cf5dfc65be04100490d646
Brandon McCray <bradmccray89@gmail.com>
2025-02-27 18:09:56 -0500

    cors update

--- src/main/java/com/brandonmccray/form_service/controller/FormController.java
+++ src/main/java/com/brandonmccray/form_service/controller/FormController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.brandonmccray.form_service.model.Form;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/forms")
+@CrossOrigin(origins = "*")
 public class FormController {
   private final FormService formService;
 
